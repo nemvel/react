cfe34f7a548af85a9beb7dc79aa9f0f68eff2165
Nemanja Velickovic <necav.kg@gmail.com>
2025-02-13 14:48:50 +0100

    test commit

--- react-crash-2024/src/App.jsx
+++ react-crash-2024/src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <HomeCards />
+      {/* TEST */}
       {/* <!-- Browse Jobs --> */}
       <section className="bg-blue-50 px-4 py-10">
         <div className="container-xl lg:container m-auto">
